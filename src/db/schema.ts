e1616836a61502dc3bfe9b719c589429cf3c5360
dipadana <dipadanadev@gmail.com>
2026-02-23 15:08:23 +0800

    chore: review the NoteSchema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,14 +1,20 @@
-import { pgTable, varchar, boolean, integer, uuid } from "drizzle-orm/pg-core";
+import { pgTable, uuid, date, varchar, numeric } from 'drizzle-orm/pg-core';
 
-export const noteTable = pgTable("notes", {
-  id: uuid().defaultRandom(),
-  title: varchar({ length: 255 }).notNull(),
-  status: boolean().default(false).notNull(),
+export const notes = pgTable('notes', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  date: date('date').notNull(),
+  vendor: varchar('vendor').notNull(),
+  name: varchar('name').notNull(),
+  amount: numeric('amount').notNull(),
+  unit: varchar('unit').notNull(),
+  price: numeric('price').notNull(),
+  category: varchar('category').notNull(),
+  totalPrice: numeric('total_price').notNull(),
 });
 
 export const userTable = pgTable("users", {
-  id: uuid().defaultRandom(),
-  name: varchar({ length: 100 }).notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-  password: varchar({ length: 255 }).notNull(),
+  id: uuid("id").primaryKey().defaultRandom(),
+  username: varchar("username", { length: 100 }).notNull(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
 });
--- src/module/note/note.entity.ts
+++ src/module/note/note.entity.ts
@@ -1,16 +1,22 @@
 import { z } from '@hono/zod-openapi'
 
 export const NoteSchema = z.object({
-  id: z.string(),
-  title: z.string(),
-  status: z.boolean(),
-  amount: z.number().optional(),
-  price: z.number()
+  id: z.uuid(),
+  date: z.iso.date(),
+  vendor: z.string().min(1),
+  name: z.string().min(1),
+  amount: z.number().positive(),
+  unit: z.string().min(1),
+  price: z.number().nonnegative(),
+  category: z.string().min(1),
+  totalPrice: z.number().nonnegative(),
 }).openapi('Note')
 
 export const NotesSchema = z.array(NoteSchema).openapi('Notes');
-export const NoteUpdateSchema = NoteSchema.omit({ id: true }).openapi('UpdateNote');
+export const NoteCreateSchema = NoteSchema.omit({ id: true }).openapi('CreateNote');
+export const NoteUpdateSchema = NoteSchema.omit({ id: true }).partial().openapi('UpdateNote');
 
 export type Note = z.infer<typeof NoteSchema>;
 export type Notes = z.infer<typeof NotesSchema>;
+export type NoteCreate = z.infer<typeof NoteCreateSchema>;
 export type NoteUpdate = z.infer<typeof NoteUpdateSchema>;
